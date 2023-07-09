ddffc1d422871367cacc8a19485dfd90d3b4d908
MerrilBds <Bdsmerril@gmail.com>
2023-07-09 11:45:40 +0200

    leçon 12 fin

--- #12.Create-complex-multi-dimensional-arrays/play.js
+++ #12.Create-complex-multi-dimensional-arrays/play.js
@@ -2,7 +2,7 @@
 let myNestedArray = [
     'level 1',   //lvl1
     ['level 2'], //lvl2
-    [['level3', 'deep']], //lvl3
+    [['level 3', 'deep']], //lvl3
     [[['level 4', 'deeper']]], //lvl4
     [[[['level 5', 'deepest']]]] //lvl5
 ];
